42fa2e7afd6b9cf0748e9615d3535c6689f112a5
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-09-28 10:24:59 -0700

    Generalize

--- test_basics.py
+++ test_basics.py
@@ -4,7 +4,7 @@ from throttler import Request, Throttler
 
 
 def test_basics() -> None:
-    throttler = Throttler(per_second=3)
+    throttler = Throttler(per_period=3)
 
     request = Request()
 
@@ -15,5 +15,24 @@ def test_basics() -> None:
 
     with freeze_time("2012-01-14T00:00:01Z"):
         for _ in range(3):
+            assert throttler.check(request)
+        assert not throttler.check(request)
+
+
+def test_longer_period() -> None:
+    throttler = Throttler(per_period=10, period_seconds=60)
+
+    request = Request()
+
+    with freeze_time("2012-01-14T00:00:00Z"):
+        for _ in range(10):
+            assert throttler.check(request)
+        assert not throttler.check(request)
+
+    with freeze_time("2012-01-14T00:00:01Z"):
+        assert not throttler.check(request)
+
+    with freeze_time("2012-01-14T00:01:00Z"):
+        for _ in range(10):
             assert throttler.check(request)
         assert not throttler.check(request)
--- throttler.py
+++ throttler.py
@@ -5,26 +5,27 @@ class Request: ...
 
 
 class Throttler:
-    def __init__(self, *, per_second: int):
-        self.per_second = per_second
+    def __init__(self, *, per_period: int, period_seconds: int = 1):
+        self.period_seconds = period_seconds
+        self.per_period = per_period
         self.requests_this_period = 0
         self.start_of_period = datetime.datetime.min
 
     def check(self, request: Request) -> bool:
         now = datetime.datetime.now()
 
-        if now - self.start_of_period >= datetime.timedelta(seconds=1):
+        if now - self.start_of_period >= datetime.timedelta(seconds=self.period_seconds):
             print(f"{now.second=} is at least one second past {self.start_of_period=}")
             self.start_of_period = now
             self.requests_this_period = 0
             print(f"... so set {self.start_of_period=} and {self.requests_this_period=}")
 
-        if self.requests_this_period == self.per_second:
-            print(f"{self.requests_this_period=} == {self.per_second=}, so returning False")
+        if self.requests_this_period == self.per_period:
+            print(f"{self.requests_this_period=} == {self.per_period=}, so returning False")
             return False
 
         print(
-            f"{self.requests_this_period=} != {self.per_second=}, so returning True, and bumping the former"
+            f"{self.requests_this_period=} != {self.per_period=}, so returning True, and bumping the former"
         )
 
         self.requests_this_period += 1
