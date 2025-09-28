529ed698e0ff05da0ebeda75baa517b53d0eb605
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-09-28 10:33:45 -0700

    Keep track of more than one request at a time

--- test_basics.py
+++ test_basics.py
@@ -6,7 +6,7 @@ from throttler import Request, Throttler
 def test_basics() -> None:
     throttler = Throttler(per_period=3)
 
-    request = Request()
+    request = Request(origin="whatever")
 
     with freeze_time("2012-01-14T00:00:00Z"):
         for _ in range(3):
@@ -22,7 +22,7 @@ def test_basics() -> None:
 def test_longer_period() -> None:
     throttler = Throttler(per_period=10, period_seconds=60)
 
-    request = Request()
+    request = Request(origin="whatever")
 
     with freeze_time("2012-01-14T00:00:00Z"):
         for _ in range(10):
@@ -36,3 +36,19 @@ def test_longer_period() -> None:
         for _ in range(10):
             assert throttler.check(request)
         assert not throttler.check(request)
+
+
+def test_distinguishes_clients() -> None:
+    throttler = Throttler(per_period=3)
+
+    freds_request = Request(origin="fred")
+    bobs_request = Request(origin="bob")
+
+    with freeze_time("2012-01-14T00:00:00Z"):
+        for _ in range(3):
+            assert throttler.check(freds_request)
+        assert not throttler.check(freds_request)
+
+        for _ in range(3):
+            assert throttler.check(bobs_request)
+        assert not throttler.check(bobs_request)
--- throttler.py
+++ throttler.py
@@ -1,34 +1,39 @@
+import collections
 import datetime
 
+Origin = str
 
-class Request: ...
+class Request:
+    def __init__(self, *, origin: Origin):
+        self.origin = origin
 
 
 class Throttler:
     def __init__(self, *, per_period: int, period_seconds: int = 1):
         self.period_seconds = period_seconds
         self.per_period = per_period
-        self.requests_this_period = 0
+        self.requests_this_period_by_origin: dict[Origin, int] = collections.defaultdict(int)
         self.start_of_period = datetime.datetime.min
 
     def check(self, request: Request) -> bool:
         now = datetime.datetime.now()
+        origin = request.origin
 
         if now - self.start_of_period >= datetime.timedelta(seconds=self.period_seconds):
             print(f"{now.second=} is at least one second past {self.start_of_period=}")
             self.start_of_period = now
-            self.requests_this_period = 0
-            print(f"... so set {self.start_of_period=} and {self.requests_this_period=}")
+            self.requests_this_period_by_origin[origin] = 0
+            print(f"... so set {self.start_of_period=} and {self.requests_this_period_by_origin[origin]=}")
 
-        if self.requests_this_period == self.per_period:
-            print(f"{self.requests_this_period=} == {self.per_period=}, so returning False")
+        if self.requests_this_period_by_origin[origin] == self.per_period:
+            print(f"{self.requests_this_period_by_origin[origin]=} == {self.per_period=}, so returning False")
             return False
 
         print(
-            f"{self.requests_this_period=} != {self.per_period=}, so returning True, and bumping the former"
+            f"{self.requests_this_period_by_origin[origin]=} != {self.per_period=}, so returning True, and bumping the former"
         )
 
-        self.requests_this_period += 1
+        self.requests_this_period_by_origin[origin] += 1
 
         return True
 
